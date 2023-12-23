0e52317e942cd196b3e1a3520114dec6b956e9c3
Kartik <s11kartik2111087@gmail.com>
2023-12-23 18:40:43 +0530

    headerandfooter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import {Container} from 'react-bootstrap'
+import Footer from './components/footer';
+import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <>
+     <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
