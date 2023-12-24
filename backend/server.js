1b19f2d334598a89bdbb0cf27ac304bff5bb92c9
Kartik <s11kartik2111087@gmail.com>
2023-12-24 15:55:04 +0530

    env Error Resolve

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
-
+dotenv.config()
 const app = express();
 
 app.get('/', (req, res) => {
@@ -15,5 +16,6 @@ app.get('/api/products/:id', (req, res) => {
     const product = products.find((p) => p._id === req.params.id)
     res.json(product)
 })
-
-app.listen(5000, console.log("Running on particular port ....."))
+const PORT = process.env.PORT
+ console.log(PORT)
+app.listen(PORT, console.log("Running on particular port ....."))
