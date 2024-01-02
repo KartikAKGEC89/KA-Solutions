9807724fa8743b7e9fe15469cd078cf1487f0f6c
Kartik <s11kartik2111087@gmail.com>
2024-01-02 16:51:57 +0530

    Add controller

---
+++ backend/controller/productController.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const Product = require('../models/productModel')
+const asynchandler = require('express-async-handler')
+
+
+const getProduct = asynchandler( async (req,res) => {
+    const product = await Product.find({})
+    
+    res.send(product);
+})
+
+const getProductId = asynchandler( async (req,res) => {
+    const product = await Product.findById(req.params.id)
+    
+    if (product) {
+        res.send(product)
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+    }
+})
+
+module.exports = {getProduct, getProductId}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,25 +1,11 @@
 const express = require('express')
-const Product = require('../models/productModel')
-const asynchandler = require('express-async-handler')
+const {getProduct, getProductId} = require('../controller/productController')
 
 const router = express.Router()
 
-router.get('/', asynchandler( async (req,res) => {
-    const product = await Product.find({})
-    
-    res.send(product);
-}))
+router.route('/').get(getProduct)
 
-router.get('/:id', asynchandler( async(req, res) => {
-    const product = await Product.findById(req.params.id)
-    
-    if (product) {
-        res.send(product)
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-}))
+router.route('/:id').get(getProductId)
 
 
 module.exports = router
