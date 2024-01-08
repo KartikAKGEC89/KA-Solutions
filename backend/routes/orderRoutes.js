59044cbceaabef3b98422eb9f28a51d5d3b9ff05
Kartik <s11kartik2111087@gmail.com>
2024-01-08 16:28:41 +0530

    feat: add new route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const authorization = require('../middleware/authMiddleware')
+const adminMiddleware = require('../middleware/adminMiddleware')
 const { orderController, getOrderbyId, updateOrderById, getMyOrderbyId } = require('../controller/orderController')
 
 
 router.route('/api/order').post(authorization, orderController)
 
 router.route('/api/order/myorder').get(authorization, getMyOrderbyId)
+
+router.route('/api/order/alluserorder').get(authorization, adminMiddleware, getMyOrderbyId)
 
 router.route('/api/order/:id').get(authorization, getOrderbyId)
 
