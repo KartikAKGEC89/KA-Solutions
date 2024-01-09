527b962b48ddf4d658be5c5aed80c148d0cfbb38
Kartik <s11kartik2111087@gmail.com>
2024-01-09 19:03:42 +0530

    feat: Product Reducer

--- frontend/src/action/productAction.js
+++ frontend/src/action/productAction.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstant"
+import { PRODUCT_DELETE_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstant"
 
 export const listProducts = () => async (dispatch) => {
 
@@ -48,4 +48,39 @@ export const detailsProducts = (id) => async (dispatch) => {
         })
     }
     
+}
+
+export const deleteProduct = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PRODUCT_DELETE_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    await axios.delete(`/api/product/${id}`, config)
+
+    setTimeout(() => {
+       dispatch({
+      type: PRODUCT_DELETE_SUCCESS
+    })
+    }, 1500)
+   
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
 }
--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -1,4 +1,4 @@
-import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstant"
+import { PRODUCT_DELETE_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstant"
 
 export const productListReducer = (state = { products: [] }, action) => {
     switch (action.type) {
@@ -24,4 +24,25 @@ export const productDetailsReducer = (state = { product: {} }, action) => {
         default:
             return state
   }
+}
+
+export const productDetailsDeleteReducer = (state = { product: {} }, action) => {
+    switch (action.type) {
+        case PRODUCT_DELETE_REQUEST:
+            return{
+        loading:true
+            }
+        case PRODUCT_DELETE_SUCCESS:
+            return {
+                loading: false,
+                product:action.payload
+            }
+        case PRODUCT_DELETE_FAIL:
+            return {
+                loading: false,
+                error:action.payloaLETE
+            }
+        default:
+            return state
+    }
 }
