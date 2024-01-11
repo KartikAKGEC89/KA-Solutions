5df59d604dfe14d5e1afb17dad37137dc78e09bc
Kartik <s11kartik2111087@gmail.com>
2024-01-11 18:27:53 +0530

    feat: Update pay and deliver

--- frontend/src/action/orderAction.js
+++ frontend/src/action/orderAction.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
-import { GET_ALL_ORDER_FAIL, GET_ALL_ORDER_REQUEST, GET_ALL_ORDER_SUCCESS, GET_MY_ORDER_FAIL, GET_MY_ORDER_REQUEST, GET_MY_ORDER_SUCCESS, GET_ORDER_FAIL, GET_ORDER_REQUEST, GET_ORDER_SUCCESS, ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS } from '../constants/orderConstants'
+import { GET_ALL_ORDER_FAIL, GET_ALL_ORDER_REQUEST, GET_ALL_ORDER_SUCCESS, GET_MY_ORDER_FAIL, GET_MY_ORDER_REQUEST, GET_MY_ORDER_SUCCESS, GET_ORDER_FAIL, GET_ORDER_REQUEST, GET_ORDER_SUCCESS, ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DELIVER_FAIL,
+  ORDER_DELIVER_SUCCESS,
+  ORDER_DELIVER_REQUEST,
+  UPDATE_ORDER_REQUEST,
+  UPDATE_ORDER_SUCCESS,
+  UPDATE_ORDER_FAIL, } from '../constants/orderConstants'
 
 
 export const placeOrderAction = (order) => async (dispatch, getState) => {
@@ -173,6 +178,77 @@ export const getAllOrderAdmin = () => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: GET_ALL_ORDER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+
+export const updateAdminPay = (order) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: UPDATE_ORDER_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(`/api/order/${order._id}/pay`, {}, config)
+
+    dispatch({
+      type: UPDATE_ORDER_SUCCESS,
+      payload:data
+    })
+  } catch (error) {
+    dispatch({
+      type: UPDATE_ORDER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const deliverOrder = (order) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_DELIVER_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(
+      `/api/order/${order._id}/delivery`,
+      {},
+      config
+    )
+
+    dispatch({
+      type: ORDER_DELIVER_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: ORDER_DELIVER_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- frontend/src/reducers/orderCartReducer.js
+++ frontend/src/reducers/orderCartReducer.js
@@ -1,4 +1,4 @@
-import { ORDER_CREATE_REQUEST, ORDER_CREATE_FAIL, ORDER_CREATE_SUCCESS, GET_ORDER_FAIL, GET_ORDER_REQUEST, GET_ORDER_SUCCESS, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_RETURN, GET_MY_ORDER_FAIL, GET_MY_ORDER_SUCCESS, GET_MY_ORDER_REQUEST, GET_MY_ORDER_RESET, GET_ALL_ORDER_REQUEST, GET_ALL_ORDER_SUCCESS, GET_ALL_ORDER_FAIL } from '../constants/orderConstants'
+import { ORDER_CREATE_REQUEST, ORDER_CREATE_FAIL, ORDER_CREATE_SUCCESS, GET_ORDER_FAIL, GET_ORDER_REQUEST, GET_ORDER_SUCCESS, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_RETURN, GET_MY_ORDER_FAIL, GET_MY_ORDER_SUCCESS, GET_MY_ORDER_REQUEST, GET_MY_ORDER_RESET, GET_ALL_ORDER_REQUEST, GET_ALL_ORDER_SUCCESS, GET_ALL_ORDER_FAIL, UPDATE_ORDER_REQUEST, UPDATE_ORDER_SUCCESS, UPDATE_ORDER_FAIL, ORDER_DELIVER_REQUEST, ORDER_DELIVER_SUCCESS, ORDER_DELIVER_FAIL, ORDER_DELIVER_RESET, UPDATE_ORDER_RESET } from '../constants/orderConstants'
 
 
 export const orderCartReducer = (state = {}, action) => {
@@ -114,6 +114,52 @@ export const getAllOrderAdminReducer =  (state = {order:[]}, action) => {
                 loading: false,
                 error:action.payload
             }
+        default:
+            return state
+    }
+}
+
+export const payReducer = (state = {}, action) => {
+    switch (action.type) {
+        case UPDATE_ORDER_REQUEST:
+            return {
+                loading:true
+            }
+        case UPDATE_ORDER_SUCCESS:
+            return {
+                loading: false,
+                success:true
+            }
+        case UPDATE_ORDER_FAIL:
+            return {
+                loading: false,
+                error:action.payload
+            }
+        case UPDATE_ORDER_RESET:
+            return {}
+        default:
+            return state
+    }
+}
+
+export const orderDeliverReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ORDER_DELIVER_REQUEST:
+            return {
+                loading: true,
+            }
+        case ORDER_DELIVER_SUCCESS:
+            return {
+                loading: false,
+                success: true,
+            }
+        case ORDER_DELIVER_FAIL:
+            return {
+                loading: false,
+                error: action.payload,
+            }
+        case ORDER_DELIVER_RESET:
+            return {}
         default:
             return state
     }
