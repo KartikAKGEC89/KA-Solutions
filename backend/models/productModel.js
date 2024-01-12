01495141aab24e5d5c1f6cd88e85df4c0db7a1e6
Kartik <s11kartik2111087@gmail.com>
2024-01-12 17:48:59 +0530

    fix: product Model

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const ReviewSchema = new mongoose.Schema({
     name: { type: String, require: true },
     rating: { type: Number, require: true },
-    Comments: { type: String, require:true}
+    Comments: { type: String, require: true },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        require: true,
+        ref: 'User'
+    }
 },
     {
         timestamps: true
