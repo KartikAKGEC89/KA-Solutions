a1d5582d210b700eb7551b96d8800933db7a9dc4
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-12-12 22:22:09 +0530

    Fix: Finally error solve

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -4,27 +4,27 @@ const path = require('path');
 const fs = require('fs');
 const router = express.Router();
 
-// Ensure the 'uploads' directory exists, if not create it
+
 const uploadsDir = path.join(__dirname, '../uploads');
 if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir, { recursive: true });
   console.log('Uploads directory created');
 }
 
-// Multer storage setup
+
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, uploadsDir); // Save files to the 'uploads' directory
+    cb(null, uploadsDir); 
   },
   filename(req, file, cb) {
     cb(
       null,
       `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    ); // Use a unique filename
+    ); 
   },
 });
 
-// Check the file type
+
 function checkFileType(file, cb) {
   const filetypes = /jpg|jpeg|png/;
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
@@ -37,7 +37,7 @@ function checkFileType(file, cb) {
   }
 }
 
-// Multer upload middleware
+
 const upload = multer({
   storage,
   fileFilter: function (req, file, cb) {
@@ -45,10 +45,10 @@ const upload = multer({
   },
 });
 
-// POST route for uploading files
+
 router.post('/', upload.single('image'), (req, res) => {
   if (req.file) {
-    // Send the uploaded file path as the response
+    
     res.send(filePath= `/uploads/${req.file.filename}`);
   } else {
     res.status(400).send({ message: 'No file uploaded' });
