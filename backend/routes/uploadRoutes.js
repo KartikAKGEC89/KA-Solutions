148278d462de51184923d6e561d42bd38726b71a
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-12-12 21:28:47 +0530

    Fix: Upload again

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
 const multer = require('multer');
-const fs = require('fs'); 
+const fs = require('fs');
 
 const router = express.Router();
+
 
 const uploadsDir = path.join(__dirname, '../uploads');
 if (!fs.existsSync(uploadsDir)) {
@@ -11,28 +12,33 @@ if (!fs.existsSync(uploadsDir)) {
     console.log('Uploads directory created');
 }
 
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
+    console.log('Destination folder:', uploadsDir);
     cb(null, uploadsDir); 
   },
   filename(req, file, cb) {
     const fileName = `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`;
-    console.log('Saving file as:', fileName);  
-    cb(null, fileName);
+    console.log('Saving file as:', fileName);  // Log the filename being saved
+    cb(null, fileName); // Name of the file
   }
 });
+
 
 function checkFileType(file, cb) {
     const filetypes = /jpg|jpeg|png/;
     const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
     const mimetype = filetypes.test(file.mimetype);
 
+    console.log('File type check:', extname, mimetype); 
     if (extname && mimetype) {
-        cb(null, true);
+        cb(null, true);  
     } else {
         cb('Image Only!');
     }
 }
+
 
 const upload = multer({
     storage,
@@ -41,10 +47,12 @@ const upload = multer({
     }
 });
 
+
 router.post('/', upload.single('image'), (req, res) => {
+    console.log('Received request to upload image');
     if (req.file) {
-        console.log('File uploaded:', req.file);  
-        res.json(`/${req.file.path}`);  
+        console.log('File uploaded:', req.file);
+        res.json(`/${req.file.path}`); 
     } else {
         res.status(400).send('No file uploaded');
     }
